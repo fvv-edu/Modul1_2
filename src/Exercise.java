d8c3d3a878474029712dab2f848655084ee2ba4e
Viktor Filatov <fvv.edu@gmail.com>
2020-08-06 11:16:09 +0300

    Variable result is deleted

--- src/Exercise.java
+++ src/Exercise.java
@@ -25,41 +25,13 @@ public class Exercise {
     }
 
     public boolean duplicate(Integer mas[]) {
-        boolean result = true;
         HashSet<Integer> hs = new HashSet<Integer>();
         for (int i=0; i<mas.length; i++) {
            if (hs.add(mas[i]) == false) {
-               result = hs.add(mas[i]);
+               return false;
            }
         }
-        return  result;
-
-        /*int counter = 0;
-        for (int i = 0; i < mas.length; i++) {
-            if (hs.contains(mas[i])) {
-                counter++;
-            }
-            hs.add(mas[i]);
-        }
-        if (counter > 0) {
-            System.out.println("Result: " + true);
-        }else {
-            System.out.println("Result: " + false);*/
-        }
-
-         /*int counter = 0;
-
-        for (int i = 0; i < mas.length; i++) {
-            for (int j = i + 1; j < mas.length; j++) {
-                if (mas[i] == mas[j]) {
-                    counter++;
-                }
-            }
-        }
-            if (counter > 0) {
-                System.out.println("Result: " + true);
-            } else {
-                System.out.println("Result: " + false);
-            }*/
+        return  true;
     }
+}
 
--- src/ExerciseRunner.java
+++ src/ExerciseRunner.java
@@ -2,7 +2,7 @@ public class ExerciseRunner {
     public static void main(String[] args) {
         Exercise ex = new Exercise();
         int [] mas1 = {1, 2, 3, 4, 8, 5, 0, 6};
-        Integer [] mas2 = {1, 2, 3, 4, 1, 7, 9, 5};
+        Integer [] mas2 = {1, 2, 3, 4, 5, 6, 7, 8};
         ex.indexSum(mas1, 5);
         System.out.println(ex.duplicate(mas2));
 
